8c594a878f9223beed3459d390940829f8c22711
Дмитрий <playcssv34@gmail.com>
2026-05-21 17:34:07 +0300

    scanner introduction

--- src/main/java/school/sorokin/javacore/Introduction/MyFirstProgram.java
+++ src/main/java/school/sorokin/javacore/Introduction/MyFirstProgram.java
@@ -1,7 +1,19 @@
 package school.sorokin.javacore.Introduction;
 
+import java.util.Scanner;
+
 public class MyFirstProgram {
     public static void main(String[] args) {
-        System.out.println("Ехали");
+        Scanner scanner = new Scanner(System.in);
+        String userInput;
+        int age;
+
+        System.out.println("Введите ваше имя: ");
+        userInput = scanner.nextLine();
+
+        System.out.println("Введите ваш возраст: ");
+        age = scanner.nextInt();
+
+        System.out.println("Привет, %s! Тебе %s лет".formatted(userInput, age));
     }
 }
